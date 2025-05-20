b2f8d57e9e3330603d342e11cc4a7076570c6b08
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2025-05-21 00:44:16 +0100

    Create side-nav app layout

---
+++ src/app/(menu)/layout.tsx
@@ -0,0 +1,20 @@
+import { SideNav } from '@/components/navigation/side-nav';
+
+export default function SidebarLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-full flex-col md:flex-row">
+      {/* Sidenav */}
+      <div className="hidden max-w-68 shrink grow md:block">
+        <SideNav></SideNav>
+      </div>
+      {/* Content */}
+      <div className="flex-1 overflow-y-auto max-md:rounded-t-xl">
+        {children}
+      </div>
+    </div>
+  );
+}
