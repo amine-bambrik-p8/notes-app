76d299dc629cca48b4a015637e399f48d469f56a
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2025-06-10 11:48:39 +0100

    Make placeholder input text more clear in dark mode

--- src/components/shared/input.tsx
+++ src/components/shared/input.tsx
@@ -56,7 +56,7 @@ export default function Input({
             ((e) => setIsInvalid(!validation?.validationFn(e.target.value)))
           }
           aria-describedby={`${name}-error`}
-          className={`text-preset-5 col-start-1 row-start-1 block w-full rounded-md border-[1px] border-neutral-300 bg-transparent py-3 text-gray-900 placeholder:text-neutral-500 focus:outline-2 focus:outline-offset-2 focus:outline-neutral-600 disabled:bg-neutral-50 dark:border-neutral-600 dark:text-neutral-100 ${prefix ? 'ps-11' : 'ps-4'} ${isInvalid && 'border-red-500'} `}
+          className={`text-preset-5 col-start-1 row-start-1 block w-full rounded-md border-[1px] border-neutral-300 bg-transparent py-3 text-gray-900 placeholder:text-neutral-500 focus:outline-2 focus:outline-offset-2 focus:outline-neutral-600 disabled:bg-neutral-50 dark:border-neutral-600 dark:text-neutral-100 dark:placeholder:text-neutral-400 ${prefix ? 'ps-11' : 'ps-4'} ${isInvalid && 'border-red-500'} `}
         />
         {prefix && (
           <div className="col-start-1 row-start-1 ms-4 flex flex-row justify-center self-center justify-self-start">
