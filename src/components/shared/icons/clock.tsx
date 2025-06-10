3d746fc59287a22ae3fb71cd8b539f102d9e08fd
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2025-06-10 17:19:58 +0100

    Create clock, delete, plus, and status svg icons

---
+++ src/components/shared/icons/clock.tsx
@@ -0,0 +1,19 @@
+export default function ClockIcon({
+  className,
+}: Readonly<{ className: string }>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      fill="none"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+    </svg>
+  );
+}
